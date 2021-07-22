531d67873985997725cd8ac37d69d741d543a5bc
haneul <msky0104@naver.com>
2021-07-22 13:28:59 +0900

    3-20

---
+++ ch3/src/ch3/Ex_20.java
@@ -0,0 +1,13 @@
+package ch3;
+
+public class Ex_20 {
+
+	public static void main(String[] args) {
+		System.out.println(-10%8);
+		System.out.println(10%-8);
+		System.out.println(-10%-8);
+
+	}
+
+}
+ // p108 % 연산자 음수의 경우
