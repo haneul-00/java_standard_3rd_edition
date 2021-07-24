c1af8af9e675ff95a74744eb41798a14cf4730eb
haneul <msky0104@naver.com>
2021-07-25 03:27:05 +0900

    4-33

---
+++ ch4/src/ch4/Ex4_33.java
@@ -0,0 +1,20 @@
+package ch4;
+
+public class Ex4_33 {
+
+	public static void main(String[] args) {
+		Loop1 : for(int i=2; i<=9; i++) {
+			for(int j=1; j<=9; j++) {
+				if(j==5)
+					break Loop1;
+				//	break;
+				//	continue Loop1;
+				//	continue;
+				System.out.println(i+"*"+j+"="+i*j);
+			}
+			System.out.println();
+		}
+	}
+}
+
+//p178 이름이 붙은 반복문
