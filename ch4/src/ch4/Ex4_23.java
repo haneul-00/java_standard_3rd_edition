5d5ac9ea09db0aa3c7f613221b935a99a61b9224
haneul <msky0104@naver.com>
2021-07-25 02:51:50 +0900

    4-23

---
+++ ch4/src/ch4/Ex4_23.java
@@ -0,0 +1,17 @@
+package ch4;
+
+public class Ex4_23 {
+	public static void main(String[] args) {
+		int i=5;
+		int j=5;
+		while(i--!=0) {
+			System.out.println(i+" - I can do it.");
+		}
+		System.out.println();
+		while(--j!=0) {
+			System.out.println(j+" - I can do it.");
+		}
+	}
+	
+}
+//p168 전위/후위 에 따른 while문 차이
