fa11a67f172720c03bfde877fe369f7a4b7340b7
haneul <msky0104@naver.com>
2021-07-28 23:10:50 +0900

    6-3

--- ch6/src/ch6/Ex6_03.java
+++ ch6/src/ch6/Ex6_03.java
@@ -1,10 +1,19 @@
 package ch6;
 
 public class Ex6_03 {
-
+		
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Tv t1=new Tv();
+		Tv t2=new Tv();
+		System.out.println("t1의 channel값은 "+ t1.channel + "입니다.");
+		System.out.println("t2의 channel값은 "+ t2.channel + "입니다.");
+		
+		t2 = t1;
+		t1.channel = 7;
+		System.out.println("t1의 channel 값을 7로 변경하였습니다.");
+		
+		System.out.println("t1의 channel값은 "+ t1.channel + "입니다.");
+		System.out.println("t2의 channel값은 "+ t2.channel + "입니다.");
 	}
-
 }
+// p238 참조변수와 인스턴스의 관계 
