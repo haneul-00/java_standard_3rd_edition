71de9e8b0fb25423d440dc4176ba542e53db475f
haneul <msky0104@naver.com>
2021-08-02 03:51:13 +0900

    7-1

---
+++ ch7/src/Ex7_01/CaptionTvTest.java
@@ -0,0 +1,34 @@
+package Ex7_01;
+
+class Tv{
+	boolean power;
+	int channel;
+	
+	void power() {power = !power;}
+	void channelUp() {channel++;}
+	void channelDown() {channel--;}
+}
+
+class CaptionTv extends Tv {
+	boolean caption;
+	void displayCaption (String text) {
+		if(caption) {
+			System.out.println(text);
+		}
+	}
+}
+
+
+public class CaptionTvTest {
+	public static void main(String[] args) {
+		CaptionTv ctv = new CaptionTv();
+		ctv.channel = 10;
+		ctv.channelUp();
+		System.out.println(ctv.channel);
+		ctv.displayCaption("hello, world1");
+		ctv.caption = true;
+		ctv.displayCaption("hello, world2");
+
+	}
+
+}
